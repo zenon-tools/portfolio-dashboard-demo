6d26d82e4348659102ef134397508c72a85371cd
vilkris <vilkris@protonmail.com>
2022-05-12 23:14:13 +0300

    Fix issue with portfolio observable

--- src/app/services/zenon-tools-api.service.ts
+++ src/app/services/zenon-tools-api.service.ts
@@ -1,17 +1,17 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { Portfolio } from './interfaces/portfolio';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { shareReplay } from "rxjs";
+import { environment } from "src/environments/environment";
+import { Portfolio } from "./interfaces/portfolio";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class ZenonToolsApiService {
-
   getPortfolio(address: string) {
-    return this.httpClient.get<Portfolio>(
-      `${environment.ztApiUrl}/portfolio?address=${address}`
-    );
+    return this.httpClient
+      .get<Portfolio>(`${environment.ztApiUrl}/portfolio?address=${address}`)
+      .pipe(shareReplay(1));
   }
 
   constructor(private httpClient: HttpClient) {}
